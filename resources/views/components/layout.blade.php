23299b40e7da3d1ea61968d37cc9b3b76706b108
dsargent25 <dsargent@wakeindustrial.com>
2024-08-26 16:53:49 -0400

    Updated changes to project

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <nav>
+        <div>
+            <a href="">
+                <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+            </a>
+        </div>
+
+        <div>links</div>
+        <div>post a job</div>
+    </nav>
+    <main>
+        {{$slot}}
+    </main>
+</body>
+</html>
